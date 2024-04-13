82ab7c855f372c1de384f4e0d31e769213d2a41b
derrickkimtai <derrickmoio92@gmail.com>
2024-04-13 23:12:47 +0300

    deploy

--- project_settings/urls.py
+++ project_settings/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('dashboard.urls')),
-    path('', include('farmers.urls')),
+    path('dashboard/', include('dashboard.urls')),
+    path('farmers/', include('farmers.urls')),
 ]
