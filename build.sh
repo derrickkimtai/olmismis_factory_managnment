d6b0171f590cf8287691a30f87a0268886e1c68d
derrickkimtai <derrickmoio92@gmail.com>
2024-04-15 20:13:29 +0300

    build

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Build the project
+echo "Building the project..."
+mvn clean install
+
+# Run the project
+echo "Running the project..."
+ pip install -r requirements.txt
+
+ python3 manage.py migrate
